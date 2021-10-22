5d555b5d3c4e1620c3617132ed19195f909160f9
Rômulo Saksida <romulo@rsaksida.com>
2021-10-22 20:47:16 -0300

    Add some fields to Image elements

--- lib/metadocs/elements/image.rb
+++ lib/metadocs/elements/image.rb
@@ -5,13 +5,15 @@ require_relative 'element'
 module Metadocs
   module Elements
     class Image < Elements::Element
-      attr_accessor :id, :content_uri, :source_uri
+      attr_accessor :id, :content_uri, :source_uri, :title, :description
 
-      def initialize(id: nil, content_uri: nil, source_uri: nil)
+      def initialize(id: nil, content_uri: nil, source_uri: nil, title: nil, description: nil)
         super()
         @id = id
         @content_uri = content_uri
         @source_uri = source_uri
+        @title = title
+        @description = description
       end
 
       def url
